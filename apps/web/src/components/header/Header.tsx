771e19be61d1102fb2cff153a34486535bbc6448
mfadlika <81882003+mfadlika@users.noreply.github.com>
2023-05-31 01:44:51 +0700

    changes requests done

--- apps/web/src/components/header/Header.tsx
+++ apps/web/src/components/header/Header.tsx
@@ -1,21 +1,21 @@
-import useClickedOutside from "@/hooks/clickedOutside";
 import { NavLink } from "@/lib";
-import Button from "@/lib/Button";
-import Card from "@/lib/Card";
 import { BrandIcon, ProfileIcon } from "@/lib/Icon";
-import List, { ListItem } from "@/lib/List";
 import { Color } from "@/types/enum";
-import { useRef, useState } from "react";
+import { Menu } from "@headlessui/react";
+import Link from "next/link";
+
+interface MenuProps {
+  id: number;
+  name: string;
+  href: string;
+}
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const wrapperRef: React.RefObject<HTMLDivElement> = useRef(null);
-
-  function handleFunction() {
-    isOpen && setIsOpen(false);
-  }
-
-  useClickedOutside(wrapperRef, { handleFunction });
+  const menuItem: MenuProps[] = [
+    { id: 1, name: "Log In / Sign Up", href: "/" },
+    { id: 2, name: "Join Us", href: "/" },
+    { id: 3, name: "Customer Service", href: "/" },
+  ];
   return (
     <header id="header">
       <div className="flex h-full w-full">
@@ -23,41 +23,32 @@ export default function Header() {
           <BrandIcon fill={Color.Teal} />
         </NavLink>
 
-        <div className="w-10/12"></div>
-        <nav className="relative h-5/6 my-auto w-2/12 md:w-1/12 flex justify-end items-center">
-          <Button
-            bgColor={Color.Teal}
-            width=""
-            height=""
-            padding="0"
-            className="w-7/12 aspect-square rounded-full flex justify-center items-center"
-            onClick={() => {
-              setIsOpen(true);
-            }}
-          >
-            <ProfileIcon height="90%" width="90%" fill="white" />
-          </Button>
-          <div
-            ref={wrapperRef}
-            className={`${
-              isOpen ? "" : "hidden"
-            } absolute top-16 z-50 bg-white border rounded-xl`}
-          >
-            <Card className="py-2" width="150px">
-              <List className="justify-evenly">
-                <ListItem className="hover:bg-teal-600 p-2">
-                  <NavLink href="/">Log In / Sign Up</NavLink>
-                </ListItem>
-                <ListItem className="hover:bg-teal-600 p-2">
-                  <NavLink href="/">Join Us</NavLink>
-                </ListItem>
-                <ListItem className="hover:bg-teal-600 p-2">
-                  <NavLink href="/">Customer Service</NavLink>
-                </ListItem>
-              </List>
-            </Card>
-          </div>
-        </nav>
+        <div className="w-10/12 z-50"></div>
+        <div className="relative h-5/6 my-auto w-2/12 md:w-1/12">
+          <Menu as="div" className="text-left">
+            <div className="w-full flex justify-end">
+              <Menu.Button className="w-7/12 aspect-square rounded-full my-auto flex justify-center items-center bg-teal-600 max-lg:w-8/12 max-md:w-7/12">
+                <ProfileIcon height="85%" width="85%" fill="white" />
+              </Menu.Button>
+            </div>
+            <Menu.Items className="absolute z-50 right-0 mt-2 w-40 origin-top-right rounded-md bg-white shadow-xl py-1">
+              {menuItem.map((menu: MenuProps) => (
+                <Menu.Item key={menu.id}>
+                  {({ active }) => (
+                    <Link
+                      href={menu.href}
+                      className={`${
+                        active ? "bg-teal-600 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {menu.name}
+                    </Link>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Menu>
+        </div>
       </div>
     </header>
   );
--- apps/web/src/components/section/main/Recommendation.tsx
+++ apps/web/src/components/section/main/Recommendation.tsx
@@ -8,61 +8,39 @@ import "swiper/css/pagination";
 import Card, { CardBody, CardImage, CardSection } from "@/lib/Card";
 import { Text, Title } from "@/lib";
 import { StarIcon } from "@/lib/Icon";
+import Button from "@/lib/Button";
 
 export default function Recommendation() {
   const size: number = 8;
-  const pageCount: number = Math.ceil(HERO_RECOMMENDATION.length / size);
 
-  const SwiperSlides = [];
-  for (let step = 0; step < pageCount; step++) {
-    SwiperSlides.push(
-      <SwiperSlide key={step}>
-        <div className="grid grid-cols-2 gap-2 m-auto pb-8 justify-center lg:grid-cols-4 lg:gap-4">
-          {HERO_RECOMMENDATION.slice(0 + step * size, size + step * size).map(
-            (props) => (
-              <Card key={props.id}>
-                <CardImage
-                  src={props.img}
-                  alt={props.alt}
-                  className="w-full aspect-square rounded-lg"
-                />
-                <CardBody>
-                  <Title className="karma-font" title={props.name} />
-                  <CardSection className="flex items-center">
-                    <StarIcon height="16px" width="16px" className="h-full" />
-                    <Text>
-                      {props.rating} · {props.reviewsCount} reviews ·{" "}
-                      {props.cityLocation}, {props.countryLocation}
-                    </Text>
-                  </CardSection>
-                </CardBody>
-              </Card>
-            )
-          )}
-        </div>
-      </SwiperSlide>
-    );
-  }
   return (
     <div className="relative h-full w-full">
       <Title
         className="w-3/12 my-5 lg:w-2/12"
         title="Our Beloved Recommendation"
       ></Title>
-      <Swiper
-        className="slide-hero"
-        pagination={{
-          clickable: true,
-        }}
-        loop={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay, Pagination]}
-      >
-        {SwiperSlides.map((props) => props)}
-      </Swiper>
+
+      <div className="grid grid-cols-2 gap-2 m-auto pb-8 justify-center lg:grid-cols-4 lg:gap-4">
+        {HERO_RECOMMENDATION.slice(0, size).map((props) => (
+          <Card key={props.id}>
+            <CardImage
+              src={props.img}
+              alt={props.alt}
+              className="w-full aspect-square rounded-lg"
+            />
+            <CardBody>
+              <Title className="karma-font" title={props.name} />
+              <CardSection className="flex items-center">
+                <StarIcon height="16px" width="16px" className="h-full" />
+                <Text>
+                  {props.rating} · {props.reviewsCount} reviews ·{" "}
+                  {props.cityLocation}, {props.countryLocation}
+                </Text>
+              </CardSection>
+            </CardBody>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
